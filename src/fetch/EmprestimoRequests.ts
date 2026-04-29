eb77e9b7e2cb2a856dab6bb4915445fec02216b4
SOPHIA ISABELLE RODRIGUES GUMBIO <sophia.gumbio@portalsesisp.org.br>
2026-04-29 15:28:27 -0300

    feat: adiciona componente de boas-vindas do sistema
    cria tela inicial com mensagem de apresentação do Acervo Digital e layout responsivo

--- src/fetch/EmprestimoRequests.ts
+++ src/fetch/EmprestimoRequests.ts
@@ -1,1 +1,35 @@
 // Classe responsável por fazer requisições à API - emprestimo
+class EmprestimoRequests {
+    private serverURL;
+    private endpointEmprestimo;
+
+    constructor() {
+        this.serverURL = 'http://localhost:3333';
+        this.endpointEmprestimo = '/api/emprestimos';
+    }
+
+    async obterListaDeEmprestimos() {
+        try {
+            const token = localStorage.getItem('token');
+
+            const respostaAPI = await fetch(`${this.serverURL}${this.endpointEmprestimo}`, {
+                headers: {
+                    'Content-Type': 'application/json',
+                    'x-access-token': `${token}`
+                }
+            });
+
+            if (respostaAPI.ok) {
+                const listaDeEmprestimos = await respostaAPI.json();
+                return listaDeEmprestimos;
+            } else {
+                throw new Error("Não foi possível listar os empréstimos.");
+            }
+        } catch (error) {
+            console.error(`Erro ao fazer a consulta de empréstimos. ${error}`);
+            return;
+        }
+    }
+}
+
+export default new EmprestimoRequests;
