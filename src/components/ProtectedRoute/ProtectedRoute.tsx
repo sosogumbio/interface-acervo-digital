92c724a0fd870fbcf12617b544b9f9b4764e78ac
SOPHIA ISABELLE RODRIGUES GUMBIO <sophia.gumbio@portalsesisp.org.br>
2026-04-29 15:01:22 -0300

    feat: adiciona proteção de rotas
    bloqueia acesso não autenticado e redireciona para login

--- src/components/ProtectedRoute/ProtectedRoute.tsx
+++ src/components/ProtectedRoute/ProtectedRoute.tsx
@@ -1,21 +1,25 @@
 import { Navigate } from 'react-router-dom';
-import AuthRequests from '../../fetch/AuthRequests';
+import { type ComponentType } from 'react';
 
 interface ProtectedRouteProps {
-    element: React.ReactElement;
+    element: ComponentType;
+    [key: string]: unknown; // representa o ...rest com tipagem genérica
 }
 
-function ProtectedRoute({ element }: ProtectedRouteProps) {
-    const isAuthenticated = localStorage.getItem('isAuth') === 'true';
-    const tokenIsValid = AuthRequests.checkTokenExpiry();
+/**
+ * Lida com a proteção das rotas
+ * A proteção previne o acesso não autorizado a rotas privadas, evitando também que a aplicação quebre.
+ * 
+ * A função recebe o elemento que será renderizado e as demais propriedades. Caso o usuário esteja autenticado, o elemento é renderizado, caso contrário, o usuário é redirecionado para a página de login.
+ * 
+ * @param element - elemento (componente) que será renderizado
+ * @param rest - demais propriedades
+ * @returns Elemento renderizado caso o usuário esteja autenticado, caso contrário, redireciona para a página de login
+ */
+const ProtectedRoute = ({ element: Element, ...rest }: ProtectedRouteProps) => {
+    const isAuthenticated = !!localStorage.getItem('isAuth');   // recupera o valor de isAuth no localstorage
 
-    // Se não está autenticado ou token expirou, redireciona para login
-    if (!isAuthenticated || !tokenIsValid) {
-        return <Navigate to="/login" replace />;
-    }
-
-    // Se está autenticado, renderiza o componente
-    return element;
-}
+    return isAuthenticated ? <Element {...rest} /> : <Navigate to="/login" />;  // verifica se o usuário está autenticado (isAuth = true), caso sim, renderiza o elemento, caso contrário, redireciona para a página de login
+};
 
 export default ProtectedRoute;
