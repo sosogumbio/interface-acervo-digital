1da3c4ae9f9b4c18c7b7fc8d4eee63c3f228aa34
SOPHIA ISABELLE RODRIGUES GUMBIO <sophia.gumbio@portalsesisp.org.br>
2026-05-06 11:09:03 -0300

    feat: configura rotas da aplicação
    define rotas com react-router-dom incluindo páginas de listagem, login, home e detalhes do aluno com parâmetro dinâmico

---
+++ src/App.tsx
@@ -0,0 +1,27 @@
+import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import PHome from './pages/PHome/PHome'
+import PLogin from './pages/PLogin/PLogin'
+import PListagemAluno from './pages/PListagem/PListagemAlunos/PListagemAlunos'
+import PListagemEmprestimo from './pages/PListagem/PListagemEmprestimo/PListagemEmprestimo'
+import PListagemLivro from './pages/PListagem/PListagemLivros/PListagemLivros'
+import PDetalhesAluno from './pages/PDetalhes/PDetalhesAluno/PDetalhesAluno'
+// import ProtectedRoute from './components/Rotas/ProtectedRoutes'
+
+function App() {
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<PHome />} />
+        <Route path='/login' element={<PLogin />} />
+        <Route path='/lista/alunos' element={<PListagemAluno />} />
+        <Route path='/lista/emprestimos' element={<PListagemEmprestimo />} />
+        <Route path='/lista/livros' element={<PListagemLivro />} />
+        <Route path='/detalhes/aluno/:id_aluno' element={<PDetalhesAluno />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
